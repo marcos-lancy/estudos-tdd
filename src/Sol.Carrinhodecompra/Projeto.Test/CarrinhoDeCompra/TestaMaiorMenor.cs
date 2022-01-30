c78fa145b7ec101c3fd7fb74487f8e1dcb8b089d
marcos william <marcos.system32@gmail.com>
2022-01-30 19:48:09 -0300

    Carrinho de Compras

---
+++ src/Sol.Carrinhodecompra/Projeto.Test/CarrinhoDeCompra/TestaMaiorMenor.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Sol.Carrinhodecompra.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Test.CarrinhoDeCompra
+{
+    [TestFixture]
+    public class TestaMaiorMenor
+    {
+        [Test]
+        public void OrdemDecrescente()
+        {
+            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+            carrinho.Adiciona(new Produto("Geladeira", 450.0M));
+            carrinho.Adiciona(new Produto("Liquidificador", 250.0M)); 
+            carrinho.Adiciona(new Produto("Jogo de pratos", 70.0M));
+
+            MaiorEMenor algoritmo = new MaiorEMenor();
+            algoritmo.Encontra(carrinho);
+
+            Assert.AreEqual("Jogo de pratos", algoritmo.Menor.Nome);
+            Assert.AreEqual("Geladeira", algoritmo.Maior.Nome);
+        }
+
+        [Test]
+        public void ApenasUmProduto()
+        {
+            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+            carrinho.Adiciona(new Produto("Geladeira", 450.0M));
+
+            MaiorEMenor algoritmo = new MaiorEMenor();
+            algoritmo.Encontra(carrinho);
+
+            Assert.AreEqual("Geladeira", algoritmo.Menor.Nome);
+            Assert.AreEqual("Geladeira", algoritmo.Maior.Nome);
+        }
+    }
+}
--- src/Sol.Carrinhodecompra/Sol.Carrinhodecompra/Models/CarrinhoDeCompras.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Sol.Carrinhodecompra.Models
-{
-    public class CarrinhoDeCompras
-    {
-        public List<Produto> Produtos { get; set; }
-    }
-}
