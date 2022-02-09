9afb84cd7aa149bf505cc0a7b9700f1d49be03b9
marcos william <marcos.system32@gmail.com>
2022-02-09 11:48:11 -0300

    numero romano

---
+++ src/Sol.Carrinhodecompra/Projeto.Test/NumeroRomano/ConversorDeNumeroromanoTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Sol.Carrinhodecompra.NumeroRomano;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Test.NumeroRomano
+{
+    [TestFixture]
+    public class ConversorDeNumeroromanoTest
+    {
+        [Test]
+        public void DeveEntenderOSimcoloI()
+        {
+            ConversorDeNumeroromano romano = new ConversorDeNumeroromano();
+
+            int numero = romano.Converte("I");
+
+            Assert.AreEqual(1, numero);
+        }
+
+        [Test]
+        public void DeveEntenderOSimboloV()
+        {
+            ConversorDeNumeroromano romano = new ConversorDeNumeroromano();
+
+            int numero = romano.Converte("V");
+
+            Assert.AreEqual(5, numero);
+        }
+    }
+}
---
+++ src/Sol.Carrinhodecompra/Sol.Carrinhodecompra/NumeroRomano/ConversorDeNumeroromano.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sol.Carrinhodecompra.NumeroRomano
+{
+    public class ConversorDeNumeroromano
+    {
+        public string Numero { get; set; }
+
+        public int Converte(string numeroEmRomano)
+        {
+            if (numeroEmRomano.Equals("I"))
+            {
+                return 1;
+            }
+            else if (numeroEmRomano.Equals("V"))
+                return 5;
+
+            return 0;
+        }
+    }
+}
